e025d9d4cbd7a3f2d4a359fa70e58722798a4eb1
Yu Wu <wuyucsu@gmail.com>
2015-05-31 10:43:10 -0500

    implement ClientHandler for server

--- CyberMusicBox/src/main/java/com/mtjwy/BeatBox/BeatBoxServer.java
+++ CyberMusicBox/src/main/java/com/mtjwy/BeatBox/BeatBoxServer.java
@@ -1,16 +1,43 @@
 package com.mtjwy.BeatBox;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class BeatBoxServer {
 	ArrayList<ObjectOutputStream> clientOutputStreams;
+	int serverPort = 4040;
 	
 	public class ClientHandler implements Runnable {
 
+		ObjectInputStream in;
+		Socket sock;
+
+		public ClientHandler(Socket clientSOcket) {
+			try {
+				sock = clientSOcket;
+				in = new ObjectInputStream(sock.getInputStream());
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
 		public void run() {
-			// TODO Auto-generated method stub
-			
+			Object o1 = null;
+			Object o2 = null;
+			try {
+				while ((o1 = in.readObject()) != null) {
+					o2 = in.readObject();
+					System.out.println("read two objects");
+					tellEveryone(o1, o2);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
 		
 	}
